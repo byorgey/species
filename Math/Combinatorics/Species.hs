9441d994c313d5bc596160a12678c896b1efb1b1
Brent Yorgey <byorgey@cis.upenn.edu>
2009-07-13 17:03:19 -0700

    more work
    Ignore-this: 2df6bfb1064ba06cf7845ec6db1b1b69
    
    darcs-hash:20090714000319-1e371-565e9f97b5d9db97fccb368d80ed828e6abd3c66

--- Math/Combinatorics/Species.hs
+++ Math/Combinatorics/Species.hs
@@ -1,9 +1,10 @@
 {-# LANGUAGE NoImplicitPrelude 
            , GADTs
            , FlexibleInstances
+           , GeneralizedNewtypeDeriving
   #-}
 
-module CombSpecies where
+module Math.Combinatorics.Species where
 
 import qualified MathObj.PowerSeries as PowerSeries
 import qualified MathObj.EGF as EGF
@@ -11,9 +12,10 @@ import qualified MathObj.EGF as EGF
 import qualified Algebra.Additive as Additive
 import qualified Algebra.Ring as Ring
 import qualified Algebra.Differential as Differential
+import qualified Algebra.Integrable as Integrable
 
 import NumericPrelude
-import PreludeBase
+import PreludeBase hiding (cycle)
 
 unimplemented :: String -> a
 unimplemented = error . (++ " is not yet implemented.")
@@ -26,18 +28,11 @@ unimplemented = error . (++ " is not yet implemented.")
 class (Differential.C s) => Species s where
   singleton :: s             -- the species X of singletons
   set       :: s             -- the species E of sets
-  list      :: s             -- the species L of linear orderings
+  cycle     :: s             -- the species C of cyclical orderings (cycles)
   o         :: s -> s -> s   -- partitional composition
   nonEmpty  :: s -> s
 
--- Some convenient synonyms.
-x, singletons, e, sets, lists :: Species s => s
-x          = singleton
-singletons = singleton
-e          = set
-sets       = set
-lists      = list
-
+-- Some convenient synonyms and derived operations.
 pointed, oneHole :: (Species s) => s -> s  
 pointed = (singleton *) . Differential.differentiate
 oneHole = Differential.differentiate
@@ -45,20 +40,42 @@ oneHole = Differential.differentiate
 madeOf :: Species s => s -> s -> s
 madeOf = o
 
+x, singletons, e, sets, cycles :: Species s => s
+x          = singleton
+singletons = singleton
+e          = set
+sets       = set
+cycles     = cycle
+
+-- Some derived species.
+list, lists :: Species s => s
+list = oneHole cycle
+lists = list
+
+octopus, octopi :: Species s => s
+octopus = cycle `o` nonEmpty lists
+octopi = octopus
+
 --------------------------------------------------------------------------------
 -- Unlabelled enumeration (classical power series) -----------------------------
 --------------------------------------------------------------------------------
 
-instance Species (PowerSeries.T Integer) where
-  singleton = PowerSeries.fromCoeffs [0,1]
-  set       = PowerSeries.fromCoeffs (repeat 1)
-  list      = PowerSeries.fromCoeffs (repeat 1)
+newtype Unlabelled = Unlabelled (PowerSeries.T Integer)
+  deriving (Additive.C, Ring.C)
+
+instance Differential.C Unlabelled where
+  differentiate = unimplemented "unlabelled differentiation"
+
+instance Species Unlabelled where
+  singleton = Unlabelled $ PowerSeries.fromCoeffs [0,1]
+  set       = Unlabelled $ PowerSeries.fromCoeffs (repeat 1)
+  cycle     = set
   o         = unimplemented "unlabelled composition"
-  nonEmpty (PowerSeries.Cons (_:xs)) = PowerSeries.Cons (0:xs)
+  nonEmpty (Unlabelled (PowerSeries.Cons (_:xs))) = Unlabelled (PowerSeries.Cons (0:xs))
   nonEmpty x = x
 
-unlabelled :: PowerSeries.T Integer -> PowerSeries.T Integer
-unlabelled = id
+unlabelled :: Unlabelled -> [Integer]
+unlabelled (Unlabelled p) = PowerSeries.coeffs p
 
 --------------------------------------------------------------------------------
 -- Species algebra -------------------------------------------------------------
@@ -72,7 +89,7 @@ data SpeciesAlg where
    (:.:)    :: SpeciesAlg -> SpeciesAlg -> SpeciesAlg
    Der      :: SpeciesAlg -> SpeciesAlg
    E        :: SpeciesAlg
-   L        :: SpeciesAlg
+   C        :: SpeciesAlg
    NonEmpty :: SpeciesAlg -> SpeciesAlg
      deriving (Show)
 
@@ -91,7 +108,7 @@ instance Differential.C SpeciesAlg where
 instance Species SpeciesAlg where
   singleton = X
   set       = E
-  list      = L
+  cycle     = C
   o         = (:.:)
   nonEmpty  = NonEmpty
 
@@ -123,28 +140,67 @@ simplify s = s
 facts :: [Integer]
 facts = 1 : zipWith (*) [1..] facts
 
--- using this math library isn't really working out all that well.
---   need to think carefully about what I need and how to design it right.
+-- This is a nice idea, but the EGF library is too slow!!
+--
+-- instance Species (EGF.T Integer) where
+--   singleton = EGF.fromCoeffs [0,1]
+--   set       = EGF.fromCoeffs $ repeat 1
+--   list      = EGF.fromCoeffs facts
+--   o         = EGF.compose
+--   nonEmpty  (EGF.Cons (_:xs)) = EGF.Cons (0:xs)
+--   nonEmpty  x = x
+--
+-- labelled :: EGF.T Integer -> [Integer]
+-- labelled = EGF.coeffs
+--
+-- Sigh.  Just compute explicitly with normal power series and
+-- zip/unzip with factorial denominators as necessary.
 
--- two libraries, or one library with two types: one for egf's and one
---   for normal power series.  Importantly it should support
---   *division* on power series with *integral* coefficients: just
---   trust that this will always be true!  or maybe do some checks.
+newtype Labelled = Labelled (PowerSeries.T Rational)
+  deriving (Additive.C, Ring.C, Differential.C)
 
-instance Species (EGF.T Integer) where
-  singleton = EGF.fromCoeffs [0,1]
-  set       = EGF.fromCoeffs $ repeat 1
-  list      = EGF.fromCoeffs facts
-  o         = EGF.compose
-  nonEmpty  (EGF.Cons (_:xs)) = EGF.Cons (0:xs)
-  nonEmpty  x = x
+instance Species Labelled where
+  singleton = Labelled $ PowerSeries.fromCoeffs [0,1]
+  set       = Labelled $ PowerSeries.fromCoeffs (map (1%) facts)
+  cycle     = Labelled $ PowerSeries.fromCoeffs (0 : map (1%) [1..])
+  o (Labelled f) (Labelled g) = Labelled $ PowerSeries.compose f g
+  nonEmpty (Labelled (PowerSeries.Cons (_:xs))) = Labelled (PowerSeries.Cons (0:xs))
+  nonEmpty x = x
 
-labelled :: EGF.T Integer -> [Integer]
-labelled = EGF.coeffs
+labelled :: Labelled -> [Integer]
+labelled (Labelled f) = map numerator . zipWith (*) (map fromInteger facts) $ PowerSeries.coeffs f
 
 --------------------------------------------------------------------------------
 -- Generation of species -------------------------------------------------------
 --------------------------------------------------------------------------------
+
+-- This seems really ugly.  Is there a better way to do this?
+
+-- data Structure a = Set [a]
+--                  | Inl (Structure a)
+--                  | Inr (Structure a)
+--                  | Pair (Structure a) (Structure a)
+--                  | Comp (Structure (Structure a))
+
+-- instance Species ([a] -> [Structure a]) where
+--   singleton []  = []
+--   singleton [x] = [Set [x]]
+--   singleton _   = []
+
+--   set es        = [Set es]
+
+--   list es       = map listToStructure (permutations es)
+
+-- listToStructure :: [a] -> Structure a
+-- listToStructure []     = Inl (Set [])
+-- listToStructure (x:xs) = Inr (Pair (Set [x]) (listToStructure xs))
+
+-- structureToList :: Structure a -> [a]
+-- structureToList (Inl _) = []
+-- structureToList (Inr (Pair (Set [x]) xs)) = x : structureToList xs
+-- structureToList _ = error "invalid list structure"                      
+
+
 
 -- newtype (f :+@ g) a = Sum  { unSum  :: Either (f a) (g a) }
 --   deriving (Show)
