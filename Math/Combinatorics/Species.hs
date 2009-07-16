a854f138e7a5ac5195d36b57e3ec590b348d197c
Brent Yorgey <byorgey@cis.upenn.edu>
2009-07-15 23:47:19 -0700

    Cycle index series
    Ignore-this: 7626b7770d80ffaa7553d1d94e5226d2
    
    darcs-hash:20090716064719-1e371-9984ac29bb09a55e3239baa2f537615ef44cf098

--- Math/Combinatorics/Species.hs
+++ Math/Combinatorics/Species.hs
@@ -171,6 +171,66 @@ labelled :: Labelled -> [Integer]
 labelled (Labelled f) = map numerator . zipWith (*) (map fromInteger facts) $ PowerSeries.coeffs f
 
 --------------------------------------------------------------------------------
+-- Cycle index series ----------------------------------------------------------
+--------------------------------------------------------------------------------
+
+instance Species (MVP.T Rational) where
+  singleton = MVP.x 1
+  set       = MVP.Cons . map partToMonomial . concatMap intPartitions $ [0..]
+
+  cycle     = undefined  -- cycles of size n are only fixed by the
+                         -- identity permutation and any permutation
+                         -- which has only one cycle of size n.
+
+  o = undefined          -- XXX
+  nonEmpty = undefined   -- XXX
+
+partToMonomial :: [(Integer, Integer)] -> Monomial.T Rational
+partToMonomial js = Monomial.Cons (zCoeff js) (M.fromList js)
+
+zCoeff :: [(Integer, Integer)] -> Rational
+zCoeff js = toRational $ 1 / aut js
+
+aut :: [(Integer, Integer)] -> FQ.T
+aut = product . map (\(b,e) -> FQ.factorial e * (fromInteger b)^e)
+
+intPartitions :: Integer -> [[(Integer, Integer)]]
+intPartitions n = intPartitions' n n
+  where intPartitions' :: Integer -> Integer -> [[(Integer,Integer)]]
+        intPartitions' 0 _ = [[]]
+        intPartitions' n 0 = []
+        intPartitions' n k =
+          [ if (j == 0) then js else (k,j):js
+            | j <- reverse [0..n `div` k]
+            , js <- intPartitions' (n - j*k) (min (k-1) (n - j*k)) ]
+
+zToLabelled :: MVP.T Rational -> Labelled
+zToLabelled (MVP.Cons xs) 
+  = Labelled . PowerSeries.fromCoeffs
+  . insertZeros
+  . concatMap (\(c,as) -> case as of { [] -> [(0,c)] ; [(1,p)] -> [(p,c)] ; _ -> [] })
+  . map (Monomial.coeff &&& (M.assocs . Monomial.powers))
+  $ xs
+
+zToUnlabelled :: MVP.T Rational -> Unlabelled
+zToUnlabelled (MVP.Cons xs)
+  = Unlabelled . PowerSeries.fromCoeffs . map numerator
+  . insertZeros
+  . map ((fst . head) &&& (sum . map snd))
+  . groupBy ((==) `on` fst)
+  . map ((sum . map (uncurry (*)) . M.assocs . Monomial.powers) &&& Monomial.coeff)
+  $ xs
+
+insertZeros :: Ring.C a => [(Integer, a)] -> [a]
+insertZeros = insertZeros' [0..]
+  where
+    insertZeros' _ [] = repeat 0
+    insertZeros' (n:ns) ((pow,c):pcs) 
+      | n < pow   = genericReplicate (pow - n) 0 
+                    ++ insertZeros' (genericDrop (pow - n) (n:ns)) ((pow,c):pcs)
+      | otherwise = c : insertZeros' ns pcs
+
+--------------------------------------------------------------------------------
 -- Generation of species -------------------------------------------------------
 --------------------------------------------------------------------------------
 
