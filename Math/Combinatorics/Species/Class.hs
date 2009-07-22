8a124903360bdacc871926be4af4d01d0c331faa
Brent Yorgey <byorgey@cis.upenn.edu>
2009-07-21 20:19:16 -0700

    add Haddocks to Species.Class
    Ignore-this: add30a47a021a51083ea9c58e3fae8be
    
    darcs-hash:20090722031916-1e371-844afd5fb58b1fe841a67a12bc4b320f15709291

--- Math/Combinatorics/Species/Class.hs
+++ Math/Combinatorics/Species/Class.hs
@@ -1,52 +1,141 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 
-module Math.Combinatorics.Species.Class where
+-- | The Species type class, which defines a small DSL for describing
+--   combinatorial species.  Other modules in this library provide
+--   specific instances which allow computing various properties of
+--   combinatorial species.
+module Math.Combinatorics.Species.Class
+    (
+      -- * The Species type class
+      Species(..)
+
+      -- * Convenience methods
+      -- $synonyms
+
+    , oneHole
+    , madeOf
+    , x
+    , e
+    , sets
+    , cycles
+
+      -- * Derived operations
+      -- $derived_ops
+
+    , pointed
+
+      -- * Derived species
+      -- $derived
+
+    , list, lists
+    , octopus, octopi
+    , partition, partitions
+
+    ) where
 
 import qualified Algebra.Differential as Differential
 
 import NumericPrelude
 import PreludeBase hiding (cycle)
 
---------------------------------------------------------------------------------
--- The class of species --------------------------------------------------------
---------------------------------------------------------------------------------
-
 infixr 5 .:
 
--- Differential requires s to be a differentiable ring.
+-- | The Species type class.  Note that the @Differential@ constraint
+--   requires s to be a differentiable ring, which means that every
+--   instance must also implement instances for "Algebra.Additive"
+--   (the species 0 and species addition, i.e. disjoint sum),
+--   "Algebra.Ring" (the species 1 and species multiplication,
+--   i.e. partitional product), and "Algebra.Differential" (species
+--   differentiation, i.e. adjoining a distinguished element).
+--
+--   Note that the 'o' operation can be used infix to suggest common
+--   notation for composition, and also to be read as an abbreviation
+--   for \"of\", as in \"top o' the mornin'\": @set \`o\` nonEmpty
+--   sets@.
 class (Differential.C s) => Species s where
-  singleton :: s             -- the species X of singletons
-  set       :: s             -- the species E of sets
-  cycle     :: s             -- the species C of cyclical orderings (cycles)
-  o         :: s -> s -> s   -- partitional composition
-  nonEmpty  :: s -> s
-  (.:)      :: s -> s -> s   -- XXX
 
--- Some convenient synonyms and derived operations.
-pointed, oneHole :: (Species s) => s -> s  
-pointed = (singleton *) . Differential.differentiate
+  -- | the species X of singletons
+  singleton :: s
+
+  -- | the species E of sets
+  set       :: s
+
+  -- | the species C of cyclical orderings (cycles/rings)
+  cycle     :: s
+
+  -- | partitional composition
+  o         :: s -> s -> s
+
+  -- | don't put a structure on the empty set
+  nonEmpty  :: s -> s
+
+  -- | @s1 .: s2@ is the species which puts an s1 structure on the
+  --   empty set and an s2 structure on anything else.  Useful for
+  --   getting recursively defined species off the ground.
+  (.:)      :: s -> s -> s
+
+-- $synonyms
+-- Some synonyms are provided for convenience.  In particular,
+-- gramatically it can often be convenient to have both the singular
+-- and plural versions of species, for example, @set \`o\` nonEmpty
+-- sets@.
+
+-- | A convenient synonym for differentiation.  F'-structures look
+--   like F-structures on a set formed by adjoining a distinguished
+--   \"hole\" element to the underlying set.
+oneHole :: (Species s) => s -> s
 oneHole = Differential.differentiate
 
+-- | A synonym for 'o' (partitional composition).
 madeOf :: Species s => s -> s -> s
 madeOf = o
 
-x, singletons, e, sets, cycles :: Species s => s
+-- | A synonym for 'singleton'.
+x :: Species s => s
 x          = singleton
-singletons = singleton
+
+-- | A synonym for 'set'.
+e :: Species s => s
 e          = set
+
+sets :: Species s => s
 sets       = set
+
+cycles :: Species s => s
 cycles     = cycle
 
--- Derived species.
-list, lists :: Species s => s
+-- $derived_ops
+-- Some derived operations on species.
+
+-- | Combinatorially, the operation of pointing picks out a
+--   distinguished element from an underlying set.  It is equivalent
+--   to the operator @x d/dx@.
+pointed :: Species s => s -> s
+pointed = (x *) . Differential.differentiate
+
+
+-- $derived
+-- Some species that can be defined in terms of the primitive species
+-- operations.
+
+-- | The species of linear orderings (lists).
+list :: Species s => s
 list  = oneHole cycle
+
+-- | A convenient synonym for 'list'.
+lists :: Species s => s
 lists = list
 
-octopus, octopi :: Species s => s
+-- | An octopus is a cyclic arrangement of lists, so called because
+--   the lists look like \"tentacles\" attached to the cyclic
+--   \"body\".
+octopi, octopus :: Species s => s
 octopus = cycle `o` nonEmpty lists
 octopi  = octopus
 
-partition, partitions :: Species s => s
+-- | The species of set partitions is just the composition E o E+,
+--   that is, sets of nonempty sets.
+partitions, partition :: Species s => s
 partition  = set `o` nonEmpty sets
 partitions = partition
 
