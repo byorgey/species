ac2823d49b64ccd88428574a61dd25a923707a56
Brent Yorgey <byorgey@cis.upenn.edu>
2009-07-22 07:21:51 -0700

    Haddocks for Unlabelled
    Ignore-this: 43d1253b7530033ad5515ce8cc833297
    
    darcs-hash:20090722142151-1e371-c71c897dcee5bc32791fbff474e1a948629358b7

--- Math/Combinatorics/Species/Unlabelled.hs
+++ Math/Combinatorics/Species/Unlabelled.hs
@@ -1,4 +1,7 @@
-module Math.Combinatorics.Species.Unlabelled where
+-- | An interpretation of species as ordinary generating functions,
+--   which count unlabelled structures.
+module Math.Combinatorics.Species.Unlabelled 
+    ( unlabelled ) where
 
 import Math.Combinatorics.Species.Types
 import Math.Combinatorics.Species.Class
@@ -29,6 +32,30 @@ instance Species Unlabelled where
 unlabelledCoeffs :: Unlabelled -> [Integer]
 unlabelledCoeffs (Unlabelled p) = PowerSeries.coeffs p
 
+-- | Extract the coefficients of an ordinary generating function as a
+--   list of Integers.  In particular, @unlabelled s !!  n@ is the
+--   number of unlabelled s-structures on an underlying set of size n.
+--   For example:
+--
+-- > > take 10 $ unlabelled octopi
+-- > [0,1,2,3,5,7,13,19,35,59]
+--
+--   gives the number of unlabelled octopi on 0, 1, 2, 3, ... 9 elements.
+--
+--   Actually, the above is something of a white lie, as you may have
+--   already realized by looking at the input type of 'unlabelled',
+--   which is 'SpeciesAlg' rather than the expected 'Unlabelled'.  The
+--   reason is that although products and sums of unlabelled species
+--   correspond to products and sums of ordinary generating functions,
+--   composition and differentiation do not!  In order to compute an
+--   ordinary generating function for a species defined in terms of
+--   composition and/or differentiation, we must compute the cycle
+--   index series for the species and then convert it to an ordinary
+--   generating function.  So 'unlabelled' actually works by first
+--   reifying the species to an AST and checking whether it uses
+--   composition or differentiation, and using operations on cycle
+--   index series if it does, and (much faster) operations directly on
+--   ordinary generating functions otherwise.
 unlabelled :: SpeciesAlg -> [Integer]
 unlabelled s 
   | hasDer s || hasComp s = unlabelledCoeffs . zToUnlabelled . reflect $ s
