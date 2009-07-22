f8698727662e25e4d4c017c7aa9c8cd3c52d14a7
Brent Yorgey <byorgey@cis.upenn.edu>
2009-07-22 12:52:46 -0700

    import Species module, which re-exports basic functionality
    Ignore-this: 13bb0cc1d2e57dc39e7c4a696d9f9d13
    
    darcs-hash:20090722195246-1e371-aa12f7783118908197ed6eccb2b84e93a00a3a63

---
+++ Math/Combinatorics/Species.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+-- | A DSL for describing combinatorial species and computing various
+--   properties. This module re-exports the most generally useful
+--   functionality; for more specialized functionality (for example,
+--   computing directly with cycle index series), see the various
+--   sub-modules.
+--
+--   Note that this library makes extensive use of the numeric-prelude
+--   library; to use it you will want to use -XNoImplicitPrelude, and
+--   import NumericPrelude and PreludeBase.
+--
+--   For a good reference (really, the only English-language
+--   reference!) on combinatorial species, see Bergeron, Labelle, and
+--   Leroux, \"Combinatorial Species and Tree-Like Structures\",
+--   Vol. 67 of the Encyclopedia of Mathematics and its Applications,
+--   Gian-Carlo Rota, ed., Cambridge University Press, 1998.
+module Math.Combinatorics.Species
+    ( -- * The combinatorial species DSL
+      Species(..)
+
+      -- ** Convenience methods
+    , oneHole
+    , madeOf
+    , x, e, sets, cycles
+          
+      -- ** Derived operations
+    , pointed
+
+      -- ** Derived species
+    , list, lists
+    , octopus, octopi
+    , partition, partitions
+
+      -- * Computing with species
+    , labelled
+    , unlabelled
+    , generate
+
+    ) where
+
+import Math.Combinatorics.Species.Class
+import Math.Combinatorics.Species.Labelled
+import Math.Combinatorics.Species.Unlabelled
+import Math.Combinatorics.Species.Generate
+  
+
