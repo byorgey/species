e779a7994e525a45aaf5cd3e31fd3f25a2a04c15
Brent Yorgey <byorgey@cis.upenn.edu>
2010-06-10 13:24:58 -0700

    a bunch more work on the simplifier
    Ignore-this: 4d56872f1eb826bfcf46580b6d41a13f
    
    darcs-hash:20100610202458-1e371-cc586ef04e7cf2771ac4ebb09f5ab9f53db7cc56

--- Math/Combinatorics/Species/Simplify.hs
+++ Math/Combinatorics/Species/Simplify.hs
@@ -14,43 +14,102 @@ import Math.Combinatorics.Species.AST
 
 import Data.Typeable
 
-simplify :: ESpeciesAST -> ESpeciesAST
-simplify (Wrap (Sized _ f)) = simplify' f
+simplify :: USpeciesAST -> USpeciesAST
+simplify UZero          = UZero
+simplify UOne           = UOne
+simplify (UN 0)         = UZero
+simplify (UN 1)         = UOne
+simplify f@(UN _)       = f
+simplify UX             =  UX
+simplify UE             =  UE
+simplify UC             =  UC
+simplify UL             =  UL
+simplify USubset        =  USubset
+simplify f@(UKSubset _) =  f
+simplify UElt           =  UElt
+simplify (f :+:% g)     = simplSum (simplify f) (simplify g)
+simplify (f :*:% g)     = simplProd (simplify f) (simplify g)
+simplify (f :.:% g)     = simplComp (simplify f) (simplify g)
+simplify (f :><:% g)    = simplCart (simplify f) (simplify g)
+simplify (f :@:% g)     = simplFunc (simplify f) (simplify g)
+simplify (UDer f)       = simplDer (simplify f)
+simplify (UOfSize f p)  = simplOfSize (simplify f) p
+simplify (UOfSizeExactly f k) = simplOfSizeExactly (simplify f) k
+simplify (UNonEmpty f)  = simplNonEmpty (simplify f)
+simplify (URec f)       = URec f
+simplify UOmega         = UOmega
 
-simplify' :: SpeciesAST f -> ESpeciesAST
-simplify' f@Zero        = wrap f
-simplify' f@One         = wrap f
-simplify' (N 0)         = wrap Zero
-simplify' (N 1)         = wrap One
-simplify' f@(N _)       = wrap f
-simplify' f@X           = wrap f
-simplify' f@E           = wrap f
-simplify' f@C           = wrap f
-simplify' f@L           = wrap f
-simplify' f@Subset      = wrap f
-simplify' f@(KSubset _) = wrap f
-simplify' f@Elt         = wrap f
-simplify' (f :+: g)     = simplSum (simplify' (stripI f)) (simplify' (stripI g))
-simplify' (f :*: g)     = simplProd (simplify' (stripI f)) (simplify' (stripI g))
+simplSum :: USpeciesAST -> USpeciesAST -> USpeciesAST
+simplSum UZero g       = g
+simplSum f UZero       = f
+simplSum UOne UOne     = UN 2
+simplSum UOne (UN n)   = UN $ succ n
+simplSum (UN n) UOne   = UN $ succ n
+simplSum (UN m) (UN n) = UN $ m + n
+simplSum (f :+:% g) h  = simplSum f (simplSum g h)
+simplSum f g           = f :+:% g
 
-simplSum :: ESpeciesAST -> ESpeciesAST -> ESpeciesAST
-simplSum (Wrap (Sized _ f)) (Wrap (Sized _ g)) = simplSum' f g
+simplProd :: USpeciesAST -> USpeciesAST -> USpeciesAST
+simplProd UZero _              = UZero
+simplProd _ UZero              = UZero
+simplProd UOne g               = g
+simplProd f UOne               = f
+simplProd (UN m) (UN n)        = UN $ m * n
+simplProd (f1 :+:% f2) g       = simplSum (simplProd f1 g) (simplProd f2 g)
+simplProd f (g1 :+:% g2)       = simplSum (simplProd f g1) (simplProd f g2)
+simplProd f (UN n)             = simplProd (UN n) f
+simplProd (UN m) (UN n :*:% g) = simplProd (UN $ m * n) g
+simplProd f ((UN n) :*:% g)    = simplProd (UN n) (simplProd f g)
+simplProd (f :*:% g) h         = simplProd f (simplProd g h)
+simplProd f g                  = f :*:% g
 
-simplSum' :: (Typeable1 f, Typeable1 g) => SpeciesAST f -> SpeciesAST g -> ESpeciesAST
-simplSum' Zero g = wrap g
-simplSum' f Zero = wrap f
-simplSum' One One = wrap (N 2)
-simplSum' One (N n) = wrap (N (succ n))
-simplSum' (N n) One = wrap (N (succ n))
-simplSum' (N m) (N n) = wrap (N (m + n))
+simplComp :: USpeciesAST -> USpeciesAST -> USpeciesAST
+simplComp UZero _        = UZero
+simplComp UOne _         = UOne
+simplComp (UN n) _       = UN n
+simplComp UX g           = g
+simplComp f UX           = f
+simplComp f UZero        = simplOfSizeExactly f 0
+simplComp (f1 :+:% f2) g = simplSum (simplComp f1 g) (simplComp f2 g)
+simplComp (f1 :*:% f2) g = simplProd (simplComp f1 g) (simplComp f2 g)
+simplComp (f :.:% g) h   = f :.:% (g :.:% h)
+simplComp f g            = f :.:% g
 
-simplProd :: ESpeciesAST -> ESpeciesAST -> ESpeciesAST
-simplProd (Wrap (Sized _ f)) (Wrap (Sized _ g)) = simplProd' f g
+simplCart :: USpeciesAST -> USpeciesAST -> USpeciesAST
+simplCart f g = f :><:% g  -- XXX
 
-simplProd' :: (Typeable1 f, Typeable1 g) => SpeciesAST f -> SpeciesAST g -> ESpeciesAST
-simplProd' Zero _ = wrap Zero
-simplProd' _ Zero = wrap Zero
-simplProd' One g  = wrap g
-simplProd' f One  = wrap f
-simplProd' (N m) (N n) = wrap (N (m * n))
-simplProd' (f1 :+: f2) g = simplSum (simplProd' (stripI f1) g) (simplProd' (stripI f2) g)
+simplFunc :: USpeciesAST -> USpeciesAST -> USpeciesAST
+simplFunc f g = f :@:% g  -- XXX
+
+simplDer :: USpeciesAST -> USpeciesAST
+simplDer UZero      = UZero
+simplDer UOne       = UZero
+simplDer (UN _)     = UZero
+simplDer UX         = UOne
+simplDer UE         = UE
+simplDer UC         = UL
+simplDer UL         = UL :*:% UL
+simplDer (f :+:% g) = simplSum (simplDer f) (simplDer g)
+simplDer (f :*:% g) = simplSum (simplProd f (simplDer g)) (simplProd (simplDer f) g)
+simplDer (f :.:% g) = simplProd (simplComp (simplDer f) g) (simplDer g)
+simplDer f          = UDer f
+
+simplOfSize :: USpeciesAST -> (Integer -> Bool) -> USpeciesAST
+simplOfSize f p = UOfSize f p  -- XXX
+
+simplOfSizeExactly :: USpeciesAST -> Integer -> USpeciesAST
+simplOfSizeExactly UZero _ = UZero
+simplOfSizeExactly UOne 0 = UOne
+simplOfSizeExactly UOne _ = UZero
+simplOfSizeExactly (UN n) 0 = UN n
+simplOfSizeExactly (UN _) _ = UZero
+simplOfSizeExactly UX 1 = UX
+simplOfSizeExactly UX _ = UZero
+simplOfSizeExactly (f :+:% g) k = simplSum (simplOfSizeExactly f k) (simplOfSizeExactly g k)
+simplOfSizeExactly (f :*:% g) k = foldr simplSum UZero
+                                    [ simplProd (simplOfSizeExactly f j) (simplOfSizeExactly g (k - j)) | j <- [0..k] ]
+ -- XXX more here? multiplication? composition?
+simplOfSizeExactly f k = UOfSizeExactly f k
+
+simplNonEmpty :: USpeciesAST -> USpeciesAST
+simplNonEmpty f = UNonEmpty f  -- XXX
