e7b337db383b22f18529b5fa812795b3831f73c6
Brent Yorgey <byorgey@cis.upenn.edu>
2010-07-14 00:38:26 -0700

    clean up NewtonRaphson
    Ignore-this: bb5cb0451f37f66425de7b378aed974b
    
    darcs-hash:20100714073826-1e371-98fc1e1b520a295866ed51d7f25f2315d85872ac

--- Math/Combinatorics/Species/NewtonRaphson.hs
+++ Math/Combinatorics/Species/NewtonRaphson.hs
@@ -1,12 +1,27 @@
 {-# LANGUAGE NoImplicitPrelude
   #-}
 
--- | Newton-Raphson's iterative method for computing with recursive
---   species.
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Math.Combinatorics.Species.CycleIndex
+-- Copyright   :  (c) Brent Yorgey 2010
+-- License     :  BSD-style (see LICENSE)
+-- Maintainer  :  byorgey@cis.upenn.edu
+-- Stability   :  experimental
+--
+-- The Newton-Raphson iterative method for computing with recursive
+-- species.  Any species @T@ which can be written in the form @T =
+-- X*R(T)@ (the species of "@R@-enriched rooted trees") may be
+-- computed by a quadratically converging iterative process.  In fact
+-- we may also compute species of the form @T = N + X*R(T)@ for any
+-- integer species @N@, by iteratively computing @T' = X*R(T' + N)@
+-- and then adding @N@.
+--
+-----------------------------------------------------------------------------
+
 module Math.Combinatorics.Species.NewtonRaphson
     (
       newtonRaphsonIter
-    , inits'
     , newtonRaphson
     , newtonRaphsonRec
     , solveForR
@@ -25,10 +40,12 @@ import Data.Typeable
 import Control.Monad (guard)
 import Data.List (delete)
 
--- | @newtonRaphson r k a@ assumes that @a@ is a species having
---   contact of order @k@ with species @t = x * (r `o` t)@ (that is, @a@
---   and @t@ agree on all label sets of size up to and including @k@),
---   and returns a new species with contact of order @2k+2@ with @t@.
+-- | A single iteration of the Newton-Raphson method.
+--   @newtonRaphsonIter r k a@ assumes that @a@ is a species having
+--   contact of order @k@ with species @t = x '*' (r ``o`` t)@ (that
+--   is, @a@ and @t@ agree on all label sets of size up to and
+--   including @k@), and returns a new species with contact of order
+--   @2k+2@ with @t@.
 --
 --   See BLL section 3.3.
 newtonRaphsonIter :: Species s => s -> Integer -> s -> s
@@ -40,22 +57,32 @@ newtonRaphsonIter r k a = a + sum as
         as = zipWith (+) ps
                (map (sum . zipWith (*) qs) $ map reverse (inits' as))
 
+-- | Lazier version of inits.
+inits' :: [a] -> [[a]]
 inits' xs = [] : inits'' xs
-inits'' []     = []
-inits'' (x:xs) = map (x:) (inits' xs)
+  where inits'' []     = []
+        inits'' (x:xs) = map (x:) (inits' xs)
 
--- | Given a species @r@ and a desired accuracy @k@, @newtonRaphson r
---   k@ computes a species which has contact at least @k@ with the
---   species @t = x * (r `o` t)@.
+-- | Given a species @r@ and a desired accuracy @k@, @'newtonRaphson'
+--   r k@ computes a species which has contact at least @k@ with the
+--   species @t = x '*' (r ``o`` t)@.
 newtonRaphson :: Species s => s -> Integer -> s
 newtonRaphson r n = newtonRaphson' 0 0
   where newtonRaphson' a k
           | k >= n = a
           | otherwise = newtonRaphson' (newtonRaphsonIter r k a) (2*k + 2)
 
+-- | @'newtonRaphsonRec' f k@ tries to compute the recursive species
+--   represented by the code @f@ up to order at least @k@, using
+--   Newton-Raphson iteration.  Returns 'Nothing' if @f@ cannot be
+--   written in the form @f = X*R(f)@ for some species @R@.
 newtonRaphsonRec :: (ASTFunctor f, Species s) => f -> Integer -> Maybe s
 newtonRaphsonRec code k = fmap (\(n,r) -> n + newtonRaphson r k) (solveForR code)
 
+-- | Given a code @f@ representing a recursive species, try to find an
+--   integer species N and species R such that @f = N + X*R(f)@.  If
+--   such species can be found, return @'Just' (N,R)@; otherwise
+--   return 'Nothing'.
 solveForR :: (ASTFunctor f, Species s) => f -> Maybe (s, s)
 solveForR code = do
   let terms = sumOfProducts . erase' $ apply code (TRec code)
@@ -68,11 +95,11 @@ solveForR code = do
                       ([N n] : ts) -> (N n, ts)
                       ts          -> (Zero, ts)
 
-  -- Now we need to be able to factor an TX out of the rest.
+  -- Now we need to be able to factor an X out of the rest.
   guard $ all (X `elem`) terms'
 
-  -- XXX this is wrong, what if there are still occurrences of TX remaining?
-  -- Now replace every recursive occurrence by (n + TX).
+  -- XXX this is wrong, what if there are still occurrences of X remaining?
+  -- Now replace every recursive occurrence by (n + X).
   let r = foldr1 (+) $ map ( foldr1 (*)
                            . map (substRec code (n + x))
                            . delete X)
